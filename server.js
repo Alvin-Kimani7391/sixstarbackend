1878f897ba7c084c69b53bd429f09db739109e05
Alvin-Kimani7391 <alvinkimani685@mail.com>
2026-08-10 23:02:45 +0300

    first commit

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const allowedOrigins = [
   'http://127.0.0.1:3000',
   'http://localhost:5500',
   'http://127.0.0.1:5500',
+  'http://www.sixstarsuppliers.com',
+  
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
