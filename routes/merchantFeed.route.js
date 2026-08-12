bd16faac2d0836eb365692ab5d1577dd6e48afb6
Alvin-Kimani7391 <alvinkimani685@mail.com>
2026-08-12 03:50:50 +0300

    first commit

--- routes/merchantFeed.route.js
+++ routes/merchantFeed.route.js
@@ -122,11 +122,11 @@ router.get('/merchant-feed.xml', async (req, res) => {
 
     const items = products.map(itemXml).filter(Boolean).join('\n');
 
-    const body = `<?xml version="1.0" encoding="UTF-8"?>
+ const body = `<?xml version="1.0" encoding="UTF-8"?>
 <rss xmlns:g="http://base.google.com/ns/1.0" version="2.0">
 <channel>
   <title>Six Star Suppliers Product Feed</title>
-  <link>${SITE_URL}</link>
+  <link>${xmlEscape(SITE_URL)}</link>
   <description>Live product feed for Six Star Suppliers, generated from the current catalog.</description>
   ${items}
 </channel>
--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'http://www.sixstarsuppliers.com',
+  'https://www.sixstarsuppliers.com',
   'https://sixstarsuppliers.com',
   process.env.CLIENT_URL,
 ].filter(Boolean);
